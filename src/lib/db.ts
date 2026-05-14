a4dad3dd6a4d8bfbd9710b49937911348a115e93
vibhu gupta <vibhugupta022@gmail.com>
2026-05-15 00:22:17 +0530

    full project done

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,8 +4,13 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient();
+export const db = new Proxy({} as PrismaClient, {
+  get(target, prop) {
+    if (!globalForPrisma.prisma) {
+      globalForPrisma.prisma = new PrismaClient();
+    }
+    return (globalForPrisma.prisma as any)[prop];
+  }
+}) as PrismaClient;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = globalForPrisma.prisma || undefined;
